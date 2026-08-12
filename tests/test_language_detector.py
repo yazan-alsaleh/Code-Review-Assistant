b3a636764379e26037ff5fcdf77e0fe83edfbd87
Yazan Jumaa <ys2110319@qu.edu.qa>
2026-08-12 16:32:44 +0300

    Add test_language_detector file and test the validator file

--- tests/test_language_detector.py
+++ tests/test_language_detector.py
@@ -1,23 +1,40 @@
-from src.review_assistant.language_detector import LanguageDetector
+# from src.review_assistant.language_detector import LanguageDetector
+from src.review_assistant.validator import Validator
+# def test_python():
 
-def test_python():
+#     code = """
+#         def hello():
+#             print("Hello")
+# """
+
+#     detector = LanguageDetector()
+
+#     assert detector.detect(code) == "Python"
+
+# def test_javascript():
+#     code = """
+# const name = "Yazan";
+# console.log(name);
+# """
+
+#     detector = LanguageDetector()
+
+#     assert detector.detect(code) == "JavaScript"
+
+def test_valid_code():
 
     code = """
         def hello():
             print("Hello")
-"""
+    """
 
-    detector = LanguageDetector()
+    language = "Python"
 
-    assert detector.detect(code) == "Python"
+    validator = Validator()
 
-def test_javascript():
-    code = """
-const name = "Yazan";
-console.log(name);
-"""
+    result = validator.validate(code, language)
+    print(result)
 
-    detector = LanguageDetector()
 
-    assert detector.detect(code) == "JavaScript"
-
+if __name__ == "__main__":
+    test_valid_code()
